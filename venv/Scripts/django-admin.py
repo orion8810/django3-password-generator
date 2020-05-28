b4ea22e886ff1e7920bb9e937cb25c2bf584be91
David Carhart <dcarhart0505@gmail.com>
2020-05-27 21:09:46 -0400

    our first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dcarh\pycharmprojects\password_generator\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
